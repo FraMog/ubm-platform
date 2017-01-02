6a20a9e7d47f4d541298370b120dac3c6877ff77
sarazaino <sarazaino7@gmail.com>
2017-01-02 20:36:05 +0100

    Aggiunti metodi per la verifica dell'autenticazione.

--- src/it/ubmplatform/autenticazione/AutenticazioneInterface.java
+++ src/it/ubmplatform/autenticazione/AutenticazioneInterface.java
@@ -1,10 +1,14 @@
 package it.ubmplatform.autenticazione;
 
+import java.util.GregorianCalendar;
+
 import it.ubmplatform.account.Account;
 
 public interface AutenticazioneInterface {
 	
 	int queryLogin(Account toSearch);
 	String queryRecuperaPassword(String email);
+	boolean queryControllaData(GregorianCalendar dataAttuale, Account trovato);
+	String queryEstraiNome(Account trovato);
 
 }
