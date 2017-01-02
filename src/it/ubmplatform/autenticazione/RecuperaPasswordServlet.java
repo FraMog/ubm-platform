878c759819c4e75818fb529993992d8194d6575a
sarazaino <sarazaino7@gmail.com>
2017-01-02 20:39:36 +0100

    Aggiunti relativi metodi.

--- src/it/ubmplatform/autenticazione/RecuperaPasswordServlet.java
+++ src/it/ubmplatform/autenticazione/RecuperaPasswordServlet.java
@@ -1,11 +1,18 @@
 package it.ubmplatform.autenticazione;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import it.ubmplatform.autenticazione.AutenticazioneInterface;
+import it.ubmplatform.factory.AbstractFactory;
+import it.ubmplatform.factory.ManagerFactory;
 
 /**
  * Servlet che gestisce il recupero di password di un utente, a cui verrà inviata tramite e-mail
@@ -13,7 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/RecuperaPasswordServlet")
 public class RecuperaPasswordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    private HttpSession session; 
+    
     public RecuperaPasswordServlet() {
         super();
     }
@@ -21,7 +29,26 @@ public class RecuperaPasswordServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//--
+		session = request.getSession();
+		
+		String email = request.getParameter("email");
+		
+		if (email.equals(""))
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("recuperaPassword.jsp");
+			rd.forward(request, response);
+		}
+		else
+		{
+			if (recuperaPassword(email))
+			{
+				//invia mail (??) contenente pass + alert invio con successo
+			}
+			else
+			{
+				//errore nell'invio
+			}
+		}
 	}
 	
 	/**
@@ -31,7 +58,13 @@ public class RecuperaPasswordServlet extends HttpServlet {
 	 * @pre email != null
 	 */
 	private boolean recuperaPassword(String email){
-		return false;
+		AbstractFactory factory = new ManagerFactory();
+		AutenticazioneInterface model = factory.createAutenticazioneManager();
+		String password = model.queryRecuperaPassword(email);
+		if (password==null)
+			return false;
+		else
+			return true;
 	}
 	
 
