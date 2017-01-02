b93c87474b6679dbc0a44b15482a80b294f6b848
sarazaino <sarazaino7@gmail.com>
2017-01-02 20:37:43 +0100

    Aggiunti relativi metodi.

--- src/it/ubmplatform/autenticazione/LogoutServlet.java
+++ src/it/ubmplatform/autenticazione/LogoutServlet.java
@@ -1,11 +1,15 @@
 package it.ubmplatform.autenticazione;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet che permette ad un utente di uscire dal sistema
@@ -14,6 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private HttpSession session;
+	
     
     public LogoutServlet() {
         super();
@@ -22,15 +28,31 @@ public class LogoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//----
+		session = request.getSession();
+		session.invalidate();
+		if (logout())
+		{
+			RequestDispatcher rs = request.getRequestDispatcher("index.jsp");
+			rs.forward(request, response);
+		}
+		else
+		{
+			session.setAttribute("logoutErrato", "true");
+			RequestDispatcher rs = request.getRequestDispatcher("index.jsp");
+			rs.forward(request, response);
+		}
 	}
 	
 	/**
 	 * Metodo che si occupa della gestione del logout, togliendo dalla sessione l'utente loggato
 	 * @return Un booleano che indica se il logout è stato effettuato correttamente
 	 */
-	private boolean logout(){
-		return false;
+	private boolean logout()
+	{
+		if ((session.getAttribute("user")==null)&&(session.getAttribute("mail")==null))
+			return true;
+		else 
+			return false;
 	}
 
 }
