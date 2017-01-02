36a172f7db754e828912e591ac2f6092944bc184
mario <mario@mario-PC>
2017-01-02 20:03:37 +0100

    modificata interfaccia

--- src/it/ubmplatform/amministrazione/AmministrazioneInterface.java
+++ src/it/ubmplatform/amministrazione/AmministrazioneInterface.java
@@ -1,13 +1,14 @@
 package it.ubmplatform.amministrazione;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
-import it.ubmplatform.profilo.Profilo;
+import it.ubmplatform.account.Account;
 
 public interface AmministrazioneInterface {
 
 	boolean queryCancellaAccount(String email);
 	boolean queryInvalidaAccount(String email);
-	boolean queryRimuoviAnnuncioNonInerente(int idAnnuncio);
-	ArrayList<Profilo> queryVisualizzaListaUtenti();
+	boolean queryRimuoviAnnuncioNonInerente(int idAnnuncio) throws SQLException;
+	public ArrayList<Account> queryVisualizzaListaUtenti() throws Exception;
 	
 }
