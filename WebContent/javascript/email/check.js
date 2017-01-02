246f54363fb8a069529b84c8df75b70991ff35b7
angelanappo93 <angnap93@gmail.com>
2017-01-02 17:22:32 +0100

    File JS per check pin di registrazione

--- WebContent/javascript/email/check.js
+++ WebContent/javascript/email/check.js
@@ -1,6 +1,11 @@
 function prova(){
 				var txt1=document.getElementById("cod");
-			
+				$.ajax({
+					  type: "POST",
+					  url: 'codiceVerifica.jsp',
+					  data: { 'codice_locale': txt1.value },
+
+					});
 				document.location.href ="codiceVerifica.jsp?codice_locale="+txt1.value; 
 				console.log(txt1.value);
 }
