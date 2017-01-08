f16b13e616c5a2f6fa876c84d91f915649cde2e5
sarazaino <sarazaino7@gmail.com>
2017-01-08 20:32:27 +0100

    Modificato costruttore.

--- src/it/ubmplatform/account/Account.java
+++ src/it/ubmplatform/account/Account.java
@@ -1,26 +1,27 @@
 package it.ubmplatform.account;
 
-import java.util.GregorianCalendar;
+import java.sql.Date;
 
 /**
  * Bean Account che rappresenta l'account all'interno del sistema. Contiene le informazioni relative all'account (email, password, tipo)
  * @author Marco
  */
 
-public final class Account {
+public class Account {
+
 	/**
-	 * Crea un account con email, password e tipo = R (regolare)
+	 * Crea un account con email, password, tipo e data di invalidazione.
 	 * @param newEmail L'email dell'account
 	 * @param pw La password dell'account
+	 * @param tipo Il tipo di account
+	 * @param dataInvalidazione L'eventuale data di invalidazione da parte dell'amministratore.
 	 */
-	
-	public Account(String newEmail, String pw){
-		email = newEmail;
-		password = pw;
-		tipo = "R";
-		dataInvalidazione = null;
+
+	public Account(String email, String password, String tipo) {
+		this.email = email;
+		this.password = password;
+		this.tipo = tipo;
 	}
-	
 
 	public String getEmail() {
 		return email;
@@ -34,24 +35,23 @@ public final class Account {
 		return tipo;
 	}
 
-	public GregorianCalendar getDataInvalidazione() {
+	public Date getDataInvalidazione() {
 		return dataInvalidazione;
-	}
-
-	public void setDataInvalidazione(GregorianCalendar dataInvalidazione) {
-		this.dataInvalidazione = dataInvalidazione;
 	}
 
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
 	
+	public void setDataInvalidazione(Date dataInvalidazione) {
+		this.dataInvalidazione = dataInvalidazione;
+	}
 
-	private GregorianCalendar dataInvalidazione;
+
 	/**
 	 * Il tipo dell'account (Bannato, Invalidato, Regolare)
 	 */
-	private String tipo;
-	private String email, password;
+	private String email, password, tipo;
+	private Date dataInvalidazione;
 
 }
