c4075eaf42b05b66842592a2789d1f50a0134473
Vincenzo Conte <vincenzoconte@hotmail.com>
2017-01-10 19:47:52 +0100

    modifiche

--- WebContent/javascript/home/caricaAnnunci.js
+++ WebContent/javascript/home/caricaAnnunci.js
@@ -1,13 +1,11 @@
 function caricaAnnunci(){
-	alert("ciao");
 	$.ajax({type:"GET",
 		url: "AnnunciRecenti",
 		success: function(risposta){ //se la richiesta ha successo
 			$('#content').html(risposta);
 		},
 		error: function (response) { //se la richiesta fallisce
-			alert("errore");
-			$('#content').htm("Non ci sono annunci da visualizzare");
+			$('#content').html("Non ci sono annunci da visualizzare");
 	    }
 	});
 }
