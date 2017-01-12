58863f4d7d091c77d2b3d8da565ab4b4dedc4d7d
mario <mario@mario-PC>
2017-01-12 18:31:19 +0100

    modifiche per funzionalida cancella e invalida da profilo utente

--- WebContent/javascript/admin/gestioneAdmin.js
+++ WebContent/javascript/admin/gestioneAdmin.js
@@ -48,3 +48,59 @@ function invalidaAccount(email){
 		});
 	});
 }
+
+
+
+
+//metodi per visualizzaprofiloAltro
+
+function cancellaAccountA(email){
+	console.log("dentro");
+	$('#cancellaAccountModal').modal("show");
+	var eliminaFeedback=false;
+	$(":checkbox").click(function(){
+		var cancellaFeedback = $(this).val();
+		if(cancellaFeedback=="true") eliminaFeedback=true;
+	});
+	$("#prosegui").click(function () {
+		console.log("eliminaFeedback"+eliminaFeedback);
+
+		$.get("CancellaAccountServlet", { "email": email, "eliminaFeedback": eliminaFeedback}, function(valore){
+			if(valore=="true") {
+
+				console.log("sono prima del refresh",valore);
+				location.replace("homePageAdmin.jsp");
+			}
+			else{
+				alert("Non è possibile eliminare l'account selezionato si è verificato un errore.")
+			}
+		}).fail(function() {
+			alert("Non è possibile eliminare l'account selezionato si è verificato un errore.");
+		});
+	});
+}	
+
+function invalidaAccountA(email){
+	console.log("dentro");
+	$('#invalidaAccountModal').modal("show");
+	var eliminaFeedback=false;
+	$(":checkbox").click(function(){
+		var cancellaFeedback = $(this).val();
+		if(cancellaFeedback=="true") eliminaFeedback=true;
+	});
+	$("#proseguiI").click(function () {
+		console.log("eliminaFeedback"+eliminaFeedback);
+
+		$.get("InvalidaAccountServlet", { "email": email, "eliminaFeedback": eliminaFeedback}, function(valore){
+			if(valore=="true") {
+				console.log("sono prima del refresh",valore);
+				location.replace("homePageAdmin.jsp");
+			}
+			else{
+				alert("Non è possibile invalidare l'account selezionato si è verificato un errore.")
+			}
+		}).fail(function() {
+			alert("Non è possibile invalidare l'account selezionato si è verificato un errore.");
+		});
+	});
+}
