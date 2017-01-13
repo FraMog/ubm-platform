5c7aa288849e2c041674cb50b2ed087ec4859f5e
Vincenzo Conte <vincenzoconte@hotmail.com>
2017-01-13 21:13:20 +0100

    piccole modifiche dbmanager

--- src/it/ubmplatform/database/DBManager.java
+++ src/it/ubmplatform/database/DBManager.java
@@ -1,24 +1,32 @@
 package it.ubmplatform.database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.apache.commons.dbcp2.BasicDataSource;
 
-public class DBManager extends BasicDataSource{
+public class DBManager {
 
-	private static DBManager datasource=new DBManager();
+	private static DBManager manager=new DBManager();
+	private static BasicDataSource datasource;
 	
 	private DBManager(){
-		super();
-		setDriverClassName("com.mysql.jdbc.Driver");
-		setUrl("jdbc:mysql://localhost/ubmplatform");
-		setUsername("root");
-		setPassword("root");
-		setMaxTotal(100);
-		setInitialSize(1);
-		setMaxIdle(20);
-		setMaxWaitMillis(60000);
+		datasource=new BasicDataSource();
+		datasource.setDriverClassName("com.mysql.jdbc.Driver");
+		datasource.setUrl("jdbc:mysql://localhost/ubmplatform");
+		datasource.setUsername("root");
+		datasource.setPassword("root");
+		datasource.setMaxTotal(100);
+		datasource.setInitialSize(1);
+		datasource.setMaxIdle(20);
+		datasource.setMaxWaitMillis(60000);
+	}
+	
+	public Connection getConnection() throws SQLException{
+		return datasource.getConnection();
 	}
 	
 	public static DBManager getInstance(){
-		return datasource;
+		return manager;
 	}
 }
